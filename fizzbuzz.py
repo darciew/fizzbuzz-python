70d64bb0a8e09d5e51c4ed39dc4a9e575b54ae63
Darcie <darciewalsh@hotmail.com>
2018-10-05 12:22:54 +0100

    Returns Fizz if number is divisible by 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,5 @@
 def FizzBuzz(number):
     if number % 15 == 0:
       return("FizzBuzz")
+    elif number % 3 == 0:
+      return("Fizz")
--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -6,6 +6,8 @@ class FizzBuzzTest(unittest.TestCase):
     def test_print_fizzbuzz_if_divisible_by_fifteen(self):
         self.assertEqual(FizzBuzz(15), "FizzBuzz")
 
+    def test_prints_fizz_if_divisible_by_three(self):
+        self.assertEqual(FizzBuzz(3), "Fizz")
 
 if __name__ == '__main__':
     unittest.main()
